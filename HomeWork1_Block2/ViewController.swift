da0dbd8820cdf00d179ec776e45e298d410582dc
Kate <k.golubtsova@gmail.com>
2022-11-12 18:11:27 +0200

    Initial Commit

---
+++ HomeWork1_Block2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork1_Block2
+//
+//  Created by Ekaterina Livinska on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
